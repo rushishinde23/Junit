f4dc6e380a5aa966f175edb3085a85ef0ddbf021
rushishinde23 <123rishinde321@gmail.com>
2025-03-06 17:14:59 +0530

    commit-2

--- src/test/java/service/CalculatorServiceTest.java
+++ src/test/java/service/CalculatorServiceTest.java
@@ -2,12 +2,31 @@ package service;
 
 
 import junit.framework.Assert;
-import org.junit.Test;
+import org.junit.*;
+
+import java.util.Date;
 
 public class CalculatorServiceTest {
 
-    @Test
-    public void addTwoNumbersTest(){
+    @BeforeClass
+    public static void init(){
+        System.out.println("Before test cases: " +new Date());
+    }
+
+    @Before
+    public  void beforeEach(){
+        System.out.println("BeforeEach TestCase");
+    }
+
+    @After
+    public void afterEach(){
+        System.out.println("AfterEach Testcase");
+    }
+
+    @Test(timeout = 2000)
+    public void addTwoNumbersTest() throws InterruptedException {
+        //Thread.sleep(3000);
+        System.out.println("Inside Add two numbers:");
         int result=CalculatorService.addTwoNumbers(10,20);
         //int expected=50;
         int expected=30;
@@ -16,6 +35,7 @@ public class CalculatorServiceTest {
 
     @Test
     public void  addAnyNumbersTest(){
+        System.out.println("Inside Add Any numbers:");
         int result=CalculatorService.addAnyNumbers(10,20,50,70);
         int expected=150;
         Assert.assertEquals(expected,result);
@@ -23,6 +43,7 @@ public class CalculatorServiceTest {
 
     @Test
     public void divideTwoNumbersTest(){
+        System.out.println("Inside Divide two numbers:");
         int result=CalculatorService.divideTwoNumbers(10,5);
         int expected=2;
         Assert.assertEquals(expected,result);
@@ -30,8 +51,14 @@ public class CalculatorServiceTest {
 
     @Test
     public void multiplyTwoNumbersTest(){
+        System.out.println("Inside Multiply two numbers:");
         int result=CalculatorService.multiplyTwoNumbers(20,4);
         int expected=80;
         Assert.assertEquals(expected,result);
+    }
+
+    @AfterClass
+    public static void after(){
+        System.out.println("After all testCases: "+new Date());
     }
 }
